30dcbe8b26a9d9c84352f04038a756351efeea2a
unknown <watb@DAUM26WS111.bqe.retail.fr>
2012-08-31 15:10:02 +0200

    genericType factory method

--- src/main/java/com/owlike/genson/GenericType.java
+++ src/main/java/com/owlike/genson/GenericType.java
@@ -2,6 +2,11 @@ package com.owlike.genson;
 
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import com.owlike.genson.reflect.TypeUtil;
 
 /**
  * This class is a holder for generic types so we can work around type erasure. You can read <a
@@ -10,7 +15,8 @@ import java.lang.reflect.Type;
  * List&lt;Integer&gt; :
  * 
  * <pre>
- * GenericType&lt;List&lt;Integer&gt;&gt; genericType = new GenericType&lt;List&lt;Integer&gt;&gt;() {};
+ * GenericType&lt;List&lt;Integer&gt;&gt; genericType = new GenericType&lt;List&lt;Integer&gt;&gt;() {
+ * };
  * List&lt;Integer&gt; listOfIntegers = new Genson().deserialize(&quot;[1,2,3]&quot;, genericType);
  * 
  * // if you want to get the standard java.lang.reflect.Type corresponding to List&lt;Integer&gt; from
@@ -21,20 +27,49 @@ import java.lang.reflect.Type;
  * 
  * @author eugen
  * 
- * @param <T> the real type
+ * @param <T>
+ *            the real type
  */
 public abstract class GenericType<T> {
 	private final Type type;
+	private final Class<T> rawClass;
 
+	@SuppressWarnings("unchecked")
 	protected GenericType() {
 		Type superType = getClass().getGenericSuperclass();
 		if (superType instanceof Class<?>) {
 			throw new IllegalArgumentException("You must specify the parametrized type!");
 		}
 		type = ((ParameterizedType) superType).getActualTypeArguments()[0];
+		rawClass = (Class<T>) TypeUtil.getRawClass(type);
+	}
+
+	private GenericType(Class<T> rawClass, Type type) {
+		this.type = type;
+		this.rawClass = rawClass;
+	}
+
+	private final static Map<Type, GenericType<?>> _genericTypesCache = new ConcurrentHashMap<Type, GenericType<?>>();
+	@SuppressWarnings("unchecked")
+	public static <T> GenericType<T> genericTypeFor(Class<T> rawClass, Type type) {
+		Class<?> typeRawClass = TypeUtil.getRawClass(type);
+		if (!rawClass.equals(typeRawClass))
+			throw new IllegalArgumentException("Argument rawClass " + rawClass
+					+ " does not match raw class " + typeRawClass + " of argument type " + type);
+		
+		GenericType<T> genericType = (GenericType<T>) _genericTypesCache.get(type);
+		if (genericType == null) {
+			genericType = new GenericType<T>(rawClass, type) {};
+			_genericTypesCache.put(type, genericType);
+		}
+		return genericType;
 	}
 
 	public Type getType() {
 		return type;
+	}
+
+	public Class<T> getRawClass() {
+		return rawClass;
 	}
 }
