34f40e4686eef0e0395b27afdd0f6735ea67174c
Nick Merlo <nicky9door@gmail.com>
2018-03-01 00:25:05 -0500

    Allow for TemporalAmounts to be formatted as objects or arrays

--- genson-java-datetime/src/main/java/com/owlike/genson/ext/javadatetime/TemporalAmountConverter.java
+++ genson-java-datetime/src/main/java/com/owlike/genson/ext/javadatetime/TemporalAmountConverter.java
@@ -4,45 +4,52 @@ import com.owlike.genson.Context;
 import com.owlike.genson.Converter;
 import com.owlike.genson.stream.ObjectReader;
 import com.owlike.genson.stream.ObjectWriter;
-import com.owlike.genson.stream.ValueType;
 
 import java.time.Duration;
 import java.time.Period;
 import java.time.temporal.TemporalAmount;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
 abstract class TemporalAmountConverter<T extends TemporalAmount> implements Converter<T> {
-	private static final List<Function<Period, Number>> PERIOD_FIELD_EXTRACTORS = Arrays.asList(
-			Period::getYears,
-			Period::getMonths,
-			Period::getDays);
+	private static final LinkedHashMap<String, Function<Period, Number>> PERIOD_FIELD_EXTRACTORS = new LinkedHashMap<>();
+	static{
+		PERIOD_FIELD_EXTRACTORS.put("years", Period::getYears);
+		PERIOD_FIELD_EXTRACTORS.put("months", Period::getMonths);
+		PERIOD_FIELD_EXTRACTORS.put("days", Period::getDays);
+	}
 
-	private static final List<BiFunction<Period, Number, Period>> PERIOD_FIELD_APPLICATORS = Arrays.asList(
-			(p, n) -> p.withYears(n.intValue()),
-			(p, n) -> p.withMonths(n.intValue()),
-			(p, n) -> p.withDays(n.intValue()) );
+	private static final LinkedHashMap<String, BiFunction<Period, Number, Period>> PERIOD_FIELD_APPLICATORS = new LinkedHashMap<>();
+	static{
+		PERIOD_FIELD_APPLICATORS.put("years", (p, n) -> p.withYears(n.intValue()));
+		PERIOD_FIELD_APPLICATORS.put("months", (p, n) -> p.withMonths(n.intValue()));
+		PERIOD_FIELD_APPLICATORS.put("days", (p, n) -> p.withDays(n.intValue()));
+	}
 
-	private static final List<Function<Duration, Number>> DURATION_FIELD_EXTRACTORS = Arrays.asList(
-			Duration::getSeconds,
-			Duration::getNano);
+	private static final LinkedHashMap<String, Function<Duration, Number>> DURATION_FIELD_EXTRACTORS = new LinkedHashMap<>();
+	static{
+		DURATION_FIELD_EXTRACTORS.put("seconds", Duration::getSeconds);
+		DURATION_FIELD_EXTRACTORS.put("nanos", Duration::getNano);
+	}
 
-	private static final List<BiFunction<Duration, Number, Duration>> DURATION_FIELD_APPLICATORS = Arrays.asList(
-			(d, n) -> d.withSeconds(n.longValue()),
-			(d, n) -> d.withNanos(n.intValue()));
+	private static final LinkedHashMap<String, BiFunction<Duration, Number, Duration>> DURATION_FIELD_APPLICATORS = new LinkedHashMap<>();
+	static{
+		DURATION_FIELD_APPLICATORS.put("seconds", (d, n) -> d.withSeconds(n.longValue()));
+		DURATION_FIELD_APPLICATORS.put("nanos", (d, n) -> d.withNanos(n.intValue()));
+	}
 
-	private boolean isAsTimestamp;
+
+	private DateTimeConverterOptions options;
 	private Function<String, T> parseFunction;
 	private Supplier<T> instanceProvider;
-	private List<Function<T, Number>> fieldExtractors;
-	private List<BiFunction<T, Number,T >> fieldApplicatiors;
+	private LinkedHashMap<String, Function<T, Number>> fieldExtractors;
+	private LinkedHashMap<String, BiFunction<T, Number,T >> fieldApplicatiors;
 
-	private TemporalAmountConverter(boolean isAsTimestamp, Function<String, T> parseFunction, Supplier<T> instanceProvider, List<Function<T, Number>> fieldExtractors, List<BiFunction<T, Number, T>> fieldApplicatiors) {
-		this.isAsTimestamp = isAsTimestamp;
+	private TemporalAmountConverter(DateTimeConverterOptions options, Function<String, T> parseFunction, Supplier<T> instanceProvider, LinkedHashMap<String, Function<T, Number>> fieldExtractors, LinkedHashMap<String, BiFunction<T, Number, T>> fieldApplicatiors) {
+		this.options = options;
 		this.parseFunction = parseFunction;
 		this.instanceProvider = instanceProvider;
 		this.fieldExtractors = fieldExtractors;
@@ -50,56 +57,92 @@ abstract class TemporalAmountConverter<T extends TemporalAmount> implements Conv
 	}
 
 	static TemporalAmountConverter<Duration> duration(DateTimeConverterOptions options){
-		return new TemporalAmountConverter<Duration>(options.isAsTimestamp(), Duration::parse, () -> Duration.ZERO, DURATION_FIELD_EXTRACTORS, DURATION_FIELD_APPLICATORS) {};
+		return new TemporalAmountConverter<Duration>(options, Duration::parse, () -> Duration.ZERO, DURATION_FIELD_EXTRACTORS, DURATION_FIELD_APPLICATORS) {};
 	}
 
 	static TemporalAmountConverter<Period> period(DateTimeConverterOptions options){
-		return new TemporalAmountConverter<Period>(options.isAsTimestamp(), Period::parse, () -> Period.ZERO, PERIOD_FIELD_EXTRACTORS, PERIOD_FIELD_APPLICATORS) {};
-	}
-
-	private List<Number> toNumbers(T object) {
-		List<Number> nums = new ArrayList<>();
-		for (Function<T, Number> fieldExtractor : fieldExtractors) {
-			nums.add(fieldExtractor.apply(object));
-		}
-		return nums;
-	}
-
-	private T fromNumbers(List<Number> numbers) {
-		T obj = instanceProvider.get();
-		for (int i = 0; i < fieldApplicatiors.size(); i++) {
-			BiFunction<T, Number, T> fieldApplicator = fieldApplicatiors.get(i);
-			Number number = numbers.get(i);
-			obj = fieldApplicator.apply(obj, number);
-		}
-		return obj;
+		return new TemporalAmountConverter<Period>(options, Period::parse, () -> Period.ZERO, PERIOD_FIELD_EXTRACTORS, PERIOD_FIELD_APPLICATORS) {};
 	}
 
 	@Override
 	public void serialize(T object, ObjectWriter writer, Context ctx) {
-		if (isAsTimestamp) {
-			List<Number> numbers = toNumbers(object);
-			writer.beginArray();
-			numbers.forEach(writer::writeValue);
-			writer.endArray();
-		} else {
+		if (options.isAsTimestamp()) {
+			switch(options.getTimestampFormat()){
+				case ARRAY:
+					writeArray(object, writer);
+					break;
+				case OBJECT:
+					writeObject(object, writer);
+					break;
+				default:
+					throw new IllegalArgumentException("Unsupported timestamp format");
+			}
+		}
+		else {
 			writer.writeValue(object.toString());
 		}
 	}
 
 	@Override
 	public T deserialize(ObjectReader reader, Context ctx) {
-		if (ValueType.ARRAY == reader.getValueType()) {
-			List<Number> numbers = new ArrayList<>();
-			reader.beginArray();
-			while (reader.hasNext()) {
-				reader.next();
-				numbers.add(reader.valueAsLong());
+		T obj;
+		if (options.isAsTimestamp()) {
+			switch(options.getTimestampFormat()){
+				case ARRAY:
+					obj = readArray(reader);
+					break;
+				case OBJECT:
+					obj = readObject(reader);
+					break;
+				default:
+					throw new IllegalArgumentException("Unsupported timestamp format");
 			}
-			reader.endArray();
-			return fromNumbers(numbers);
-		} else {
-			return parseFunction.apply(reader.valueAsString());
 		}
+		else {
+			obj = parseFunction.apply(reader.valueAsString());
+		}
+
+		return obj;
+	}
+
+	private void writeArray(T object, ObjectWriter writer) {
+		writer.beginArray();
+		for(Function<T, Number> fieldExtractor: fieldExtractors.values()){
+			writer.writeValue(fieldExtractor.apply(object));
+		}
+		writer.endArray();
+	}
+
+	private T readArray(ObjectReader reader) {
+		T obj = instanceProvider.get();
+		reader.beginArray();
+		for(BiFunction<T, Number, T> fieldApplicator: fieldApplicatiors.values()){
+			reader.next();
+			obj = fieldApplicator.apply(obj, reader.valueAsLong());
+		}
+		reader.endArray();
+		return obj;
+	}
+
+	private void writeObject(T object, ObjectWriter writer) {
+		writer.beginObject();
+		for(Map.Entry<String, Function<T, Number>> fieldExtractor: fieldExtractors.entrySet()){
+			writer.writeName(fieldExtractor.getKey());
+			writer.writeValue(fieldExtractor.getValue().apply(object));
+		}
+		writer.endObject();
+	}
+
+	private T readObject(ObjectReader reader) {
+		T obj = instanceProvider.get();
+		reader.beginObject();
+		while(reader.hasNext()){
+			reader.next();
+			String applicatorName = reader.name();
+			BiFunction<T, Number, T> applicator = fieldApplicatiors.get(applicatorName);
+			obj = applicator.apply(obj, reader.valueAsLong());
+		}
+		reader.endObject();
+		return obj;
 	}
 }
--- genson-java-datetime/src/test/java/com/owlike/genson/ext/javadatetime/DurationTest.java
+++ genson-java-datetime/src/test/java/com/owlike/genson/ext/javadatetime/DurationTest.java
@@ -8,8 +8,8 @@ import java.time.Duration;
 
 public class DurationTest extends JavaDateTimeTestBase {
 	@Test
-	public void testTimestampSerialization() {
-		Genson genson = createTemporalAmountTimestampGenson(Duration.class, TimestampFormat.MILLIS);
+	public void testTimestampSerializationArray() {
+		Genson genson = createTemporalAmountTimestampGenson(Duration.class, TimestampFormat.ARRAY);
 		long seconds = 321L;
 		long nanos = 123456789;
 		Duration duration = Duration.ofSeconds(seconds, nanos);
@@ -17,13 +17,34 @@ public class DurationTest extends JavaDateTimeTestBase {
 	}
 
 	@Test
-	public void testTimestampDeserialization() {
-		Genson genson = createTemporalAmountTimestampGenson(Duration.class, TimestampFormat.MILLIS);
+	public void testTimestampDeserializationArray() {
+		Genson genson = createTemporalAmountTimestampGenson(Duration.class, TimestampFormat.ARRAY);
 		long seconds = 321L;
 		long nanos = 123456789;
 		Duration duration = Duration.ofSeconds(seconds, nanos);
 		Assert.assertEquals(duration, genson.deserialize(toJsonArray(seconds, nanos), Duration.class));
 	}
+
+	@Test
+	public void testTimestampSerializationObject() {
+		Genson genson = createTemporalAmountTimestampGenson(Duration.class, TimestampFormat.OBJECT);
+		long seconds = 321L;
+		long nanos = 123456789;
+		Duration duration = Duration.ofSeconds(seconds, nanos);
+		String json = "{\"seconds\":321,\"nanos\":123456789}";
+		Assert.assertEquals(json, genson.serialize(duration));
+	}
+
+	@Test
+	public void testTimestampDeserializationObject() {
+		Genson genson = createTemporalAmountTimestampGenson(Duration.class, TimestampFormat.OBJECT);
+		long seconds = 321L;
+		long nanos = 123456789;
+		Duration duration = Duration.ofSeconds(seconds, nanos);
+		String json = "{\"seconds\":321,\"nanos\":123456789}";
+		Assert.assertEquals(duration, genson.deserialize(json, Duration.class));
+	}
+
 
 	@Test
 	public void testStringSerialization() {
